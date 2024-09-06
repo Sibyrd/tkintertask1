f222f32421984f6948e8c3eb3b13030424a650a3
Sibyrd <mail@theobird.com>
2024-09-06 11:49:54 +0100

    initial commit

---
+++ multiply.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Multiplication Program")
+window.geometry("500x200")
+title = tk.Label(window, text="Multiplication")
+title.pack(pady=20)
+
+window.mainloop()
