5d6f5b96ee55bd5996f7684e35df882c52ed56f7
Sibyrd <49642941+Sibyrd@users.noreply.github.com>
2024-09-06 11:50:11 +0100

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,9 +1,23 @@
 import tkinter as tk
 
 window = tk.Tk()
+
+
+def multiply():
+    title.configure(text=str(int(inp1.get())*int(inp2.get())))
+
 window.title("Multiplication Program")
-window.geometry("500x200")
+window.geometry("300x100")
 title = tk.Label(window, text="Multiplication")
-title.pack(pady=20)
+inp1 = tk.Entry(window)
+inp2 = tk.Entry(window)
+button = tk.Button(window, text="Multiply!", command=multiply)
+title.pack()
+inp1.pack()
+inp2.pack()
+button.pack()
+
+
+
 
 window.mainloop()
